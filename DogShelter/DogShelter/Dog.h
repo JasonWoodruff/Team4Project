b1bf2695747084f0a8839a7c2b948efa19d7b560
WheelrS <stacy.h.wheeler@gmail.com>
2014-11-20 11:03:23 -0800

    Update Dog.h
    setter and getter methods are defined inline for now.  I'll change that to a Dog.cpp file once I get more familiar with GitHub.
    The default constructor will generate an ID of the format "DOG" + padding zeros + keyNumGenerator.
    keyNumGenerator will be initialized with the count of Dogs being read in from the file (a counter will need to be added to the file I/O logic)

--- DogShelter/DogShelter/Dog.h
+++ DogShelter/DogShelter/Dog.h
@@ -2,21 +2,70 @@
 #define DOG_H
 #include <string>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 class Dog
 {
 private:
-	string ID; //format DOG001, DOG002
+	static int keyNumGenerator = 1;     //increments when each new instance of Dog is created
+	
+	string ID;                          //format DOG001, DOG002
 	string name;
 	string age;
 	string breed;
 	string description;
+	
+protected:
+	string generateID()
+	{
+		int tempID = keyNumGenerator++;
+		string IDstr = itoa(tempID);
+		
+		//creates a "padding" of zeros for the id
+		while(IDstr.length() < 4)
+			IDstr += "0";
+			
+		ID = "DOG" + IDstr;
+	}
 
 public:
-	Dog();
-	Dog(string strID, string strN, string strAge, string strB, string strD);
+	Dog()
+	{  ID = generateID();  }
+	
+	Dog(string strID, string strN, string strAge, string strB, string strD):
+		ID(strID), name(strN), age(strAge), breed(strB), description(strD)
+	
+	string getID();
+	{  return ID;  }
+	
+	string getName()
+	{  return name;  }
+	
+	string getAge()
+	{  return age;  }
+	
+	string getBreed()
+	{  return breed;  }
+	
+	string getDescription()
+	{  return description;  }
+	
+	void setID(string key)
+	{  ID = key;  }
+	
+	void setName(string n)
+	{  name = n;  }
+	
+	void setAge(string a)
+	{  age = a;  }
+	
+	void setBreed(string type)
+	{  breed = type;  }
+	
+	void setDescription(string desc)
+	{  description = desc;  }
 
 };
 
